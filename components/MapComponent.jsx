657237e940819ec7ced826139910deb3c88e2d4a
Omar El Helbawy <omarelhelbawy@192.168.1.6>
2026-08-28 18:41:41 +0300

    revert(map): restore native tile parameters to remove artificial upscale blur

--- components/MapComponent.jsx
+++ components/MapComponent.jsx
@@ -178,13 +178,10 @@ const MapComponent = ({ originCoords, destCoords, progress, liveTelemetry, isCam
         attributionControl={false}
         style={{ width: '100%', height: '100%', background: '#090a0f' }}
       >
-        {/* Ultra Crisp High-Resolution Esri Dark Gray Map Tiles */}
+        {/* Clean Native Resolution Esri Dark Gray Map Tiles */}
         <TileLayer
           url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
-          tileSize={512}
-          zoomOffset={-1}
-          maxNativeZoom={16}
-          maxZoom={19}
+          maxZoom={16}
         />
 
         {/* Flight path curve as dotted line */}
